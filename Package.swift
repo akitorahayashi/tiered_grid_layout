506579d8a4bba17ff67762fb6634ed5a456236f9
terrio32 <atora.has75@gmail.com>
2025-05-11 11:39:46 +0900

    TieredGridLayoutTestsのpathをPackage.swiftに記載

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         ),
         .testTarget(
             name: "TieredGridLayoutTests",
-            dependencies: ["TieredGridLayout"]
+            dependencies: ["TieredGridLayout"],
+            path: "TieredGridLayoutTests"
         ),
     ]
 )
